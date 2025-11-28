66cc79e3ff6b0f30b0af47178a98fe3d29ef6526
kanta1207 <kan0712livepool@gmail.com>
2025-11-27 20:51:38 -0800

    feat: setup monorepo

--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vitest/config'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    viteReact(),
-  ],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-  },
-})
+	plugins: [
+		viteTsConfigPaths({
+			projects: ["./tsconfig.json"],
+		}),
+		viteReact(),
+	],
+	test: {
+		environment: "jsdom",
+		globals: true,
+	},
+});
